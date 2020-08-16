72f5d4fcfe8612aceb8834f5f2d9052ec665437d
puremoru <e155745@ie.u-ryukyu.ac.jp>
2020-08-16 22:42:34 +0900

    fix

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,18 +1,16 @@
 class RequirementsController < ApplicationController
   skip_before_action :authorize_request, only: [:index, :show]
-  before_action :get_requirement, only: [:show]
-  before_action :get_current_company, only: [:create]
 
   def index
     render json: { requirements: Requirement.all.order(created_at: :desc) }, status: 200
   end
 
   def show 
-    render json: { requirement: @requirement }, status: 200
+    render json: { requirement: requirement }, status: 200
   end
 
   def create
-    requirement = @current_company.requirements.new(requirement_params)
+    requirement = @current_user.company.requirements.new(requirement_params)
     if requirement.save
       render json: { message: 'Created' }, status: 201
     else 
@@ -29,16 +27,7 @@ class RequirementsController < ApplicationController
     )
   end
 
-  def get_requirement
-    @requirement = Requirement.find(params[:id])
-  end
-
-  def get_current_company
-    @current_company = @current_user.company
-    if @current_company
-      return @current_company
-    else
-      return render json: { errors: 'Company does not exist.' }, status: 401
-    end
+  def requirement
+    Requirement.find(params[:id])
   end
 end
