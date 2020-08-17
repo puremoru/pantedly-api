7b5f087213022c25dab14e4f7d0614dff78618cd
puremoru <e155745@ie.u-ryukyu.ac.jp>
2020-08-17 19:06:41 +0900

    userのvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   belongs_to :company, optional: true
   has_secure_password
+
+  presence :name, presence: true
+  presence :email, presence: true, uniqueness: true
 end
