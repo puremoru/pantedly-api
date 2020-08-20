e15640d74446cac24170211b1ba50cc24c454b39
puremoru <e155745@ie.u-ryukyu.ac.jp>
2020-08-21 00:15:48 +0900

    求職者の一覧を取得するAPIを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+  before_action :is_recruiter, only: [:index]
+  def index
+    render json: { users: User.where(company_id: nil) }, status: 200 # TODO: 並び順をどうするか考える
+  end
+
   def update
     if @current_user.update(user_params)
       render json: { message: 'Updated' }, status: 200
     else
       render json: { message: 'Bad Request' }, status: 400
     end
-    
   end
 
   private
@@ -16,5 +20,11 @@ class UsersController < ApplicationController
       :password,
       :about,
     )
+  end
+
+  def is_recruiter
+    if !@current_user.company
+      return render json: { message: 'Forbidden' }, status: 403
+    end
   end
 end
